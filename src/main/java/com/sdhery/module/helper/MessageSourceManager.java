161f974e41fe2e00f58ebcc4c0d08cc85fb91825
sdhery@gmail.com <herys@163.com>
2013-06-09 15:09:48 +0800

    提交修改

---
+++ src/main/java/com/sdhery/module/helper/MessageSourceManager.java
@@ -0,0 +1,19 @@
+package com.sdhery.module.helper;
+
+import org.springframework.context.support.AbstractMessageSource;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sdhery
+ * Date: 13-6-9
+ * Time: 下午3:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageSourceManager {
+    public static String setRegisterReturnMessage(String key, HttpServletRequest request){
+        AbstractMessageSource abstractMessageSource = (AbstractMessageSource) SpringContextHolder.getBean("messageSource");
+        return abstractMessageSource.getMessage(key, new Object[0], request.getLocale());
+    }
+}
